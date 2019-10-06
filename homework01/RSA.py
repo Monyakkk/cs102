e2a7418824bf5db38773ede7ee1e52b44831c05f
Monyakkk <kom_sasha2001@mail.ru>
2019-10-06 17:49:12 +0300

    Добавлена функция Is_prime

--- homework01/RSA.py
+++ homework01/RSA.py
@@ -11,7 +11,19 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    else:
+        f = 0
+        for i in range(2, n + 1):
+            if n % i == 0:
+                f += 1
+        if f == 1:
+            return True
+        else:
+            return False
     pass
 
 
