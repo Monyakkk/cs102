4ad6e91a49508d663a018124f8dcaa624ec6ae85
Monyakkk <kom_sasha2001@mail.ru>
2019-10-08 22:35:05 +0300

    Добавлена поддержка различных кодировок

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,14 +11,14 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for i in range (len(plaintext)):
         x = ord(plaintext[i])
         y = ord(keyword[i % len(keyword)])
-        if 65 <= y <= 90:
-            y -= 65
-        if 97 <= y <= 122:
-            y -= 97
-        if 65 <= x <= 90:
-            ciphertext += chr(((x - 65 + y) % 26) + 65)
-        elif 97 <= x <= 122:
-            ciphertext += chr((x - 97 + y) % 26 + 97)
+        if "A" <= keyword[i % len(keyword)] <= "Z":
+            y -= ord("A")
+        if "a" <= keyword[i % len(keyword)] <= "z":
+            y -= ord("a")
+        if "A" <= plaintext[i] <= "Z":
+            ciphertext += chr(((x - ord("A") + y) % 26) + ord("A"))
+        elif "a" <= plaintext[i] <= "z":
+            ciphertext += chr((x - ord("a") + y) % 26 + ord("a"))
         else:
             ciphertext += chr(x)
     return ciphertext
@@ -37,14 +37,14 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     for i in range (len(ciphertext)):
         x = ord(ciphertext[i])
         y = ord(keyword[i % len(keyword)])
-        if 65 <= y <= 90:
-            y -= 65
-        if 97 <= y <= 122:
-            y -= 97
-        if 65 <= x <= 90:
-            plaintext += chr(((x - 65 - y) % 26) + 65)
-        elif 97 <= x <= 122:
-            plaintext += chr((x - 97 - y) % 26 + 97)
+        if "A" <= keyword[i % len(keyword)] <= "Z":
+            y -= ord("A")
+        if "a" <= keyword[i % len(keyword)] <= "z":
+            y -= ord("a")
+        if "A" <= ciphertext[i] <= "Z":
+            plaintext += chr(((x - ord("A") - y) % 26) + ord("A"))
+        elif "a" <= ciphertext[i] <= "z":
+            plaintext += chr((x - ord("a") - y) % 26 + ord("a"))
         else:
             plaintext += chr(x)
     return plaintext
