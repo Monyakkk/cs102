850f2ee12fb47ee5f591295c2554c14d2687cc58
Monyakkk <kom_sasha2001@mail.ru>
2019-10-21 22:47:11 +0300

    Оптимизация функции get_col()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -60,10 +60,10 @@ def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    col = pos[1]
+
     L=[]
     for i in range(len(grid[0])):
-        L.append(grid[i][col])
+        L.append(grid[i][pos[1]])
     return L
 
 def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
